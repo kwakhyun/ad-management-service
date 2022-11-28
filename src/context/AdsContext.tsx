ff373e8df0700bfd79831d2af7cdc04e3cd19122
kwakhyun <khyun2348@naver.com>
2022-11-28 21:56:44 +0900

    Feat: context api

---
+++ src/context/AdsContext.tsx
@@ -0,0 +1,56 @@
+import React, { useContext, createContext, useEffect, useState, useMemo } from 'react';
+import { AdsService } from '../service/AdsService';
+import { Advertisement, DropdownOption } from '../types';
+import { adManagementOptions } from '../utils/conts';
+
+type AdsProviderProps = {
+  children: React.ReactNode;
+  adsService: AdsService;
+};
+
+type State = {
+  ads: Advertisement[];
+  editAd: (edited: Advertisement) => void;
+  filterAd: (adOption: DropdownOption) => void;
+  currentOption: DropdownOption;
+};
+
+const AdsContext = createContext<State | null>(null);
+export const useAds = () => useContext(AdsContext);
+
+export function AdsProvider({ children, adsService }: AdsProviderProps) {
+  const [adsRaw, setAdsRaw] = useState<Advertisement[]>([]);
+  const [ads, setAds] = useState<Advertisement[]>([]);
+  const [currentOption, setCurrentOption] = useState<DropdownOption>(adManagementOptions[0]);
+
+  const filterAd = (adOption: DropdownOption) => {
+    setCurrentOption(adOption);
+
+    if (adOption.option === 'all') {
+      return setAds(adsRaw);
+    }
+
+    setAds(adsRaw.filter((ad) => ad.status === adOption.option));
+  };
+
+  const editAd = (edited: Advertisement) => {
+    const editedAds = ads.map((ad) => (ad.id === edited.id ? edited : ad));
+    setAds(editedAds);
+  };
+
+  const getAds = async () => {
+    const data = await adsService.get();
+    setAdsRaw(data);
+    setAds(data);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      getAds();
+    }, 500);
+  }, []);
+
+  const value = useMemo(() => ({ ads, editAd, filterAd, currentOption }), [ads, currentOption]);
+
+  return <AdsContext.Provider value={value}>{children}</AdsContext.Provider>;
+}
