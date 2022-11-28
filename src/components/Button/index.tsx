850b2e647a979f0c467986996ea9f2e2de77b1ba
kwakhyun <khyun2348@naver.com>
2022-11-28 21:59:07 +0900

    Feat: components

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,27 @@
+import styled, { DefaultTheme, FlattenInterpolation, ThemeProps } from 'styled-components';
+
+type ButtonProps = {
+  children: React.ReactNode;
+  customStyle?: FlattenInterpolation<ThemeProps<DefaultTheme>>;
+  onClick?: () => void;
+};
+
+function Button({ children, customStyle, onClick }: ButtonProps) {
+  return (
+    <ButtonContainer type="button" customStyle={customStyle} onClick={onClick}>
+      {children}
+    </ButtonContainer>
+  );
+}
+
+export default Button;
+
+const ButtonContainer = styled.button<{ customStyle: FlattenInterpolation<ThemeProps<DefaultTheme>> | undefined }>`
+  padding: 12px 20px;
+  background-color: ${({ theme }) => theme.primary};
+  border-radius: 10px;
+  font-size: 14px;
+  font-weight: 700;
+  color: white;
+  ${({ customStyle }) => customStyle}
+`;
